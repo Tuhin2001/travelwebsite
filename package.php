b45601b2142a395216bb74c96f12c8da66e06272
Tuhin Chowdhury <111597074+Tuhin2001@users.noreply.github.com>
2024-06-03 20:57:33 +0530

    Add files via upload

---
+++ package.php
@@ -0,0 +1,274 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Travel Agency :: Best Agency</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+<!-- 
+   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+   <script>
+      $(document).ready(function(){
+          $(".scroll-top").click(function() {
+              $("html, body").animate({ 
+                  scrollTop: 0 
+              }, "slow");
+              return false;
+          });
+      });
+   </script> -->
+
+<!-- CHAT BOT -->
+<script type="text/javascript">
+        (function(d, m){
+            var kommunicateSettings =
+                {"appId":"1e09b51788dd98db478a141f31644695a","popupWidget":true,"automaticChatOpenOnNavigation":true};
+            var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+            var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+            window.kommunicate = m; m._globals = kommunicateSettings;
+        })(document, window.kommunicate || {});
+    /* NOTE : Use web server to view HTML files as real-time update will not work if you directly open the HTML file in the browser. */
+    </script>
+    
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo"><img src="images/logo.jpg"></a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php" class="active">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Dubai Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our Dubai top selling packages!</p>
+            <h2>Rs 10,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Delhi Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our Delhi top selling packages!</p>
+            <h2>Rs 9,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Japan Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our Japan top selling packages!</p>
+            <h2>Rs 11,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Australia Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our Australia top selling packages!</p>
+            <h2>Rs 13,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>china Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our china top selling packages!</p>
+            <h2>Rs 7,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>singapur Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our singapur top selling packages!</p>
+            <h2>Rs 12,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-7.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>spain Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our spain top selling packages!</p>
+            <h2>Rs 18,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-8.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>canada Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our canada top selling packages!</p>
+            <h2>Rs 21,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-9.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>bali Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our bali top selling packages!</p>
+            <h2>Rs 14,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-10.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>nepal Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our nepal top selling packages!</p>
+            <h2>Rs 7,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>bangladesh Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our bangladesh top selling packages!</p>
+            <h2>Rs 5,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-12.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Maldives Tour Packages</h3>
+            <p>Enjoy the Emirates with unforgettable fun with our Maldives top selling packages!</p>
+            <h2>Rs 21,900/-</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">see more</span></div>
+<!-- <button type="button" class="scroll-top"><i class="fa fa-angle-double-up" aria-hidden="true"></i></button> -->
+
+</section>
+
+<!-- packages section ends -->
+<!-- footer section starts  -->
+
+<section class="footer">
+   <div class="box-container">
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+      </div>
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +880-1517-089144 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-2222-333333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> Travel2024@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> West Bengal, Kol-700122</a>
+      </div>
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+      </div>
+   </div>
+   <div class="credit"> designed by <span>Group 13</span> | all rights reserved! </div>
+</section>
+
+<!-- footer section ends -->
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
